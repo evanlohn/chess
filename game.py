bf45fffe9bc69b3267e14f5a5d4524462c6c5c0e
evanlohn <evan.lohn@berkeley.edu>
2020-05-20 13:33:55 -0700

    nicer interface (code and UI) for board updates, can play a game now if you ignore en passant, castling, winning, losing, and drawing

--- game.py
+++ game.py
@@ -1,40 +1,97 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-from board import all_ranks, all_files, WHITE, BLACK, Board, Move
+from board import all_ranks, all_files, WHITE, BLACK, Board, Move, other
 
-def click_handler(r_ind, c_ind, board):
+def click_handler(r_ind, c_ind, game):
 	def handle_click(event):
-		print('square at row {} col {} was clicked'.format(r_ind, c_ind))
+		
+		grid_row, grid_col = game.bcoords2gcoords(r_ind, c_ind)
+		clicked_frame = game.board_frame.grid_slaves(row=grid_row, column=grid_col)[0]
+		if game.selected is None:
+			piece = game.get_piece(r_ind, c_ind)
+			if piece and piece.color == game.turn:
+				game.select_sq(r_ind, c_ind, clicked_frame)
+		else:
+			r_sel, c_sel = game.selected
+			grid_r_sel, grid_c_sel = game.bcoords2gcoords(r_sel, c_sel)
+			sel_frame = game.board_frame.grid_slaves(row=grid_r_sel, column=grid_c_sel)[0]
+			if not (r_sel == r_ind and c_sel == c_ind):
+				# abstraction to deal with castling and en passant
+				mv = game.create_move(game.selected, (r_ind, c_ind))
+				if mv.is_valid():
+					mv.make_move()
+					game.check_finished()
+					game.turn = other(game.turn)
+					game.update_all()
+				game.selected = None
+			game.deselect_sq(sel_frame)
+
+
+		
+		#print('square at row {} col {} was clicked'.format(r_ind, c_ind))
+		#print('square {}{} clicked'.format(all_files[c_ind], all_ranks[r_ind]))
 	return handle_click
 
 class Game:
 
 	def __init__(self, board):
 		self.board = board
+
+		## Tkinter stuff
 		self.window = tk.Tk()
 		self.board_frame = tk.Frame(master=self.window, width=400, height=400, bg="red")
 		self.info_frame = tk.Frame(master=self.window, width=200, bg="blue")
 		self.board_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 		self.info_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-
-		self.turn = WHITE # white goes first
 		self.all_pimgs = {} #PhotoImage cache
 
+
+		## state used for turns
+		self.turn = WHITE # white goes first
+		self.selected = None # no piece (square) selected
+
+
+		letter_frame = tk.Frame(master=self.board_frame, bg='white')
+		letter_frame.grid(row=0, column=0) # blank corner
+		letter_frame = tk.Frame(master=self.board_frame, bg='white')
+		letter_frame.grid(row=len(self.board.board_lst), column=0) # blank corner
+		## put pieces on board
 		for r_ind, row in enumerate(self.board.board_lst):
-			self.board_frame.columnconfigure(r_ind, weight=1, minsize=75) #, minsize=50
-			self.board_frame.rowconfigure(r_ind, weight=1, minsize=75)
+			self.board_frame.columnconfigure(r_ind + 1, weight=1, minsize=75) #, minsize=50
+			self.board_frame.rowconfigure(r_ind + 1, weight=1, minsize=75)
 			for c_ind, piece in enumerate(row):
-				color = "saddle brown" if (r_ind + c_ind) % 2 == 1 else "navajo white"
-				handler = click_handler(r_ind, c_ind, self.board)
+				if r_ind == 0:
+					letter_frame = tk.Frame(master=self.board_frame, bg='white')
+					letter_frame.grid(row=0, column=c_ind + 1)
+					letter_lbl = tk.Label(master=letter_frame, text=all_files[c_ind])
+					letter_lbl.pack(fill=tk.BOTH, expand=True)
+				if c_ind == 0:
+					letter_frame = tk.Frame(master=self.board_frame, bg='white')
+					letter_frame.grid(row=r_ind + 1, column=0)
+
+					letter_lbl = tk.Label(master=letter_frame, text=all_ranks[r_ind])
+					letter_lbl.pack(fill=tk.BOTH, expand=True)
+					letter_frame.lbl = letter_lbl
+
+				if r_ind == len(self.board.board_lst) - 1:
+					letter_frame = tk.Frame(master=self.board_frame, bg='white')
+					letter_frame.grid(row=len(self.board.board_lst) + 1, column=c_ind + 1)
+					letter_lbl = tk.Label(master=letter_frame, text=all_files[c_ind])
+					letter_lbl.pack(fill=tk.BOTH, expand=True)
+
+				color_switch = 0 if self.turn == WHITE else 1
+				color = "saddle brown" if (r_ind + c_ind) % 2 == color_switch else "navajo white"
+				handler = click_handler(r_ind, c_ind, self)
 				frame = tk.Frame(master=self.board_frame, relief=tk.GROOVE, borderwidth=1, bg=color)
 				frame.bg = color
+				frame.displayed_label = None
 				frame.bind("<Button-1>", handler)
-				frame.grid(row=r_ind, column=c_ind, sticky="nsew")
+				frame.grid(row=r_ind + 1, column=c_ind + 1, sticky="nsew")
 
-				if piece is not None:
-					self.add_piece(piece, (r_ind, c_ind))
+				#self.update_square(r_ind, c_ind)
 
 
+		self.update_all()
 				# TODO: at some point, add letters/numbers on the outside
 				#if (r_ind == len(self.board.board_lst) - 1):
 				#	mini_text = tk.Label(master=frame, text=all_files[c_ind])
@@ -49,20 +106,68 @@ class Game:
 			return render
 		return self.all_pimgs[key]
 
+	def get_piece(self, r, c):
+		return self.board.board_lst[r][c]
 
-	def add_piece(self, piece, loc):
-		frame = self.board_frame.grid_slaves(row=loc[0], column=loc[1])[0]
-		img = self.get_piece_img(piece)
-		handler = click_handler(loc[0], loc[1], self.board)
-		label = tk.Label(master=frame, image=img, bg=frame.bg)
-		label.photo = img
-		label.bind("<Button-1>", handler)
-		label.pack(fill=tk.BOTH, expand=True)
+	def bcoords2gcoords(self, row, col):
+		grid_row = row + 1 if self.turn == BLACK else len(all_ranks) - row
+		return grid_row, col + 1
+
+	def update_square(self, row, col):
+		piece = self.board.board_lst[row][col]
+
+
+		grid_row, grid_col = self.bcoords2gcoords(row, col)
+		frame = self.board_frame.grid_slaves(row=grid_row, column=grid_col)[0]
+
+		color_switch = 0 #if self.turn == BLACK else 1
+		color = "saddle brown" if (row + col) % 2 == color_switch else "navajo white"
+		frame.config(bg=color)
+		frame.bg = color
+
+		handler = click_handler(row, col, self)
+		frame.bind("<Button-1>", handler)
+
+		if frame.displayed_label is not None:
+			frame.displayed_label.pack_forget()
+			frame.displayed_label.destroy()
+			frame.displayed_label = None
+
+		if piece is not None:
+			img = self.get_piece_img(piece)
+			label = tk.Label(master=frame, image=img, bg=frame.bg)
+			label.photo = img
+			label.bind("<Button-1>", handler)
+			label.pack(fill=tk.BOTH, expand=True)
+
+			frame.displayed_label = label
+
+	def update_all(self):
+		for r_ind, row in enumerate(self.board.board_lst):
+			frame = self.board_frame.grid_slaves(row=r_ind + 1, column=0)[0]
+			letter_ind = r_ind if self.turn == BLACK else len(all_ranks) - r_ind - 1
+			frame.lbl.config(text=all_ranks[letter_ind])
+			for c_ind, piece in enumerate(row):
+				self.update_square(r_ind, c_ind)
+
+	def select_sq(self, r_ind, c_ind, frame):
+		self.selected = (r_ind, c_ind)
+		frame.config(highlightthickness=2)
+		frame.config(highlightbackground='red')
+
+	def deselect_sq(self, frame):
+		self.selected = None
+		frame.config(highlightthickness=0)
+
+	def create_move(self, src, dst):
+		#TODO: code to handle castling?
+		return Move(src, dst, self.board)
+
+	def check_finished(self):
+		return None
 
 	def display_board(self):
 		self.window.mainloop()
-
-
 
 
 class Player:
